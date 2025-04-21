860c8021b4bb07c3904c4a477177e843509ac061
Janaloka0 <marwansuhendra200@gmail.com>
2025-04-21 10:44:13 +0700

    Pertemuan 4

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,9 @@ Route::get('backend/laporan/formproduk', [ProdukController::class, 'formProduk']
 Route::post('backend/laporan/cetakproduk', [ProdukController::class, 'cetakProduk'])
 ->name('backend.laporan.cetakproduk')->middleware('auth');
 
+// Route untuk Customer
+Route::resource('backend/customer', CustomerController::class, ['as' => 'backend'])->middleware('auth');
+
 // Frontend
 Route::get('/beranda', [BerandaController::class, 'index'])->name('beranda');
 
@@ -65,4 +68,4 @@ Route::get('/produk/all', [ProdukController::class, 'produkAll'])->name('produk.
 Route::get('/auth/redirect', [CustomerController::class, 'redirect'])->name('auth.redirect');
 Route::get('/auth/google/callback', [CustomerController::class, 'callback'])->name('auth.callback');
 // Logout
-Route::post('/logout', [CustomerController::class, 'logout'])->name('logout');
+Route::post('/logout', [CustomerController::class, 'logout'])->name('customer.logout');
