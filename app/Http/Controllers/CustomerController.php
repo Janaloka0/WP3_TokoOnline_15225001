40de0cc02d602aa170299e686c21921279d9a96a
Janaloka0 <marwansuhendra200@gmail.com>
2025-04-21 11:38:18 +0700

    pertemuan 4

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,31 +11,18 @@ use Illuminate\Support\Facades\Hash;
 
 class CustomerController extends Controller
 {
-    public function index()
-    {
-        $customer = Customer::orderBy('id', 'desc')->get();
-            return view('backend.v_customer.index', [
-            'judul' => 'Customer',
-            'sub' => 'Halaman Customer',
-            'index' => $customer
-        ]);
-    }
-
     // Redirect ke Google
     public function redirect()
     {
         return Socialite::driver('google')->redirect();
     }
-
-        // Callback dari Google
+    // Callback dari Google
     public function callback()
     {
         try {
             $socialUser = Socialite::driver('google')->user();
-
             // Cek apakah email sudah terdaftar
             $registeredUser = User::where('email', $socialUser->email)->first();
-
             if (!$registeredUser) {
                 // Buat user baru
                 $user = User::create([
@@ -43,23 +30,20 @@ class CustomerController extends Controller
                     'email' => $socialUser->email,
                     'role' => '2', // Role customer
                     'status' => 1, // Status aktif
-                    'password' => Hash::make('default_password'), // Password default (opsional)
+                    'password' => Hash::make('default_password'), // Password default(opsional)
                 ]);
-
                 // Buat data customer
                 Customer::create([
                     'user_id' => $user->id,
                     'google_id' => $socialUser->id,
                     'google_token' => $socialUser->token
                 ]);
-
                 // Login pengguna baru
                 Auth::login($user);
             } else {
                 // Jika email sudah terdaftar, langsung login
                 Auth::login($registeredUser);
             }
-
             // Redirect ke halaman utama
             return redirect()->intended('beranda');
         } catch (\Exception $e) {
@@ -72,7 +56,16 @@ class CustomerController extends Controller
         Auth::logout(); // Logout pengguna
         $request->session()->invalidate(); // Hapus session
         $request->session()->regenerateToken(); // Regenerate token CSRF
-
         return redirect('/')->with('success', 'Anda telah berhasil logout.');
+    }
+
+    public function index()
+    {
+        $customer = Customer::orderBy('id', 'desc')->get();
+        return view('backend.v_customer.index', [
+            'judul' => 'Customer',
+            'sub' => 'Halaman Customer',
+            'index' => $customer
+        ]);
     }
 }
